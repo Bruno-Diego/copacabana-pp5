0ae83e6aa86f9d62c1dc35175c3af49f70d44f3d
BrunoDS <bruno.diego.x@hotmail.com>
2023-01-22 10:11:16 +0000

    Add deployment configurations

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn copacabana.wsgi:application
--- copacabana/settings.py
+++ copacabana/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['copacabana-jewellery.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -129,9 +129,17 @@ WSGI_APPLICATION = 'copacabana.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
