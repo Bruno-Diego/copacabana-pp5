579ac9913b263f971a1e3c74fa9f6ef106c2d868
BrunoDS <bruno.diego.x@hotmail.com>
2023-02-28 07:18:27 +0000

    Remove argument for 500 error page

--- copacabana/views.py
+++ copacabana/views.py
@@ -16,6 +16,6 @@ def handler400(request, exception):
     return render(request, "errors/400.html", status=400)
 
 
-def handler500(request, exception):
+def handler500(request):
     """ Error Handler 500 - Internal Server Error """
     return render(request, "errors/500.html", status=500)
