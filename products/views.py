ba46e230cec6980cbb21b12e4641101e553cb4a7
BrunoDS <bruno.diego.x@hotmail.com>
2023-03-01 04:58:37 +0000

    Refactor reviews template and views to show only related reviews

--- products/views.py
+++ products/views.py
@@ -68,7 +68,7 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
-    reviews = Review.objects.all()
+    reviews = Review.objects.all().filter(product_title=product)
     context = {
         'product': product,
         'reviews': reviews,
